dc069782abecb2d2090b96e4bf3e996df4c986dc
andre-carbajal <carbajalvargasandre@gmail.com>
2026-08-25 15:23:30 -0500

    Update archives name format and rename mod name to "Go Fish Rehooked"

--- common/src/main/java/net/anvian/gofish/GoFishConstants.java
+++ common/src/main/java/net/anvian/gofish/GoFishConstants.java
@@ -12,7 +12,7 @@ public final class GoFishConstants {
     private GoFishConstants() {}
 
     public static final String MOD_ID = "gofish";
-    public static final String MOD_NAME = "Go Fish";
+    public static final String MOD_NAME = "Go Fish Rehooked";
     public static final String MOD_VERSION = "2.0.0";
     public static final ResourceKey<CreativeModeTab> ITEM_GROUP =
             ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(MOD_ID, "group"));
